3fad12807ba78684e9e48a4850b02f82d85a9898
Stephen Fiser <stephen.fiser@gmail.com>
2013-09-01 12:07:41 -0500

    rerouted app

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,37 +3,49 @@ require 'spec_helper'
 describe "Static pages" do
 	describe "Home page" do
 		it "Should have the h1 'Sample App'" do 
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_selector('h1', text: 'Sample App')
 		end
 
-		it "Should have the title 'Sample App | Home" do
-			visit '/static_pages/home'
+		it "Should have the title 'Sample App | Home'" do
+			visit root_path
 			page.should have_selector('title', text: 'Sample App | Home')
 		end
 	end
 
 	describe "Help page" do
 		it "Should have the h1 'Help'" do 
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('h1', text: 'Help')
 		end
 
-		it "Should have the title 'Sample App | Help" do
-			visit '/static_pages/help'
+		it "Should have the title 'Sample App | Help'" do
+			visit help_path
 			page.should have_selector('title', text: 'Sample App | Help')
 		end
 	end
 
 	describe "About page" do
 		it "Should have the h1 'About Us'" do 
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('h1', text: 'About Us')
 		end
 
-		it "Should have the title 'Sample App | About Us" do
-			visit '/static_pages/about'
+		it "Should have the title 'Sample App | About Us'" do
+			visit about_path
 			page.should have_selector('title', text: 'Sample App | About Us')
+		end
+	end
+
+	describe "Contact page" do
+		it "Should have the h1 'Contact Us'" do 
+			visit contact_path
+			page.should have_selector('h1', text: 'Contact Us')
+		end
+
+		it "Should have the title 'Sample App | Contact Us'" do
+			visit contact_path
+			page.should have_selector('title', text: 'Sample App | Contact Us')
 		end
 	end
 end
